de8bd8b2b32dd374d1a325b9793c40d856f38c72
Andreas Schaeffer <andreas@schaeffernet.de>
2024-12-30 00:14:01 +0100

    Initial POC

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use ollama_rs::generation::completion::request::GenerationRequest;
+use ollama_rs::Ollama;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ollama = Ollama::default();
+
+    let model = "llama2:latest".to_string();
+    let prompt = "Why is the sky blue?".to_string();
+
+    match ollama.generate(GenerationRequest::new(model, prompt)).await {
+        Ok(response) => {
+            println!("{}", response.response);
+            Ok(())
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+            Err(e.into())
+        }
+    }
+}
